36dbc73fef69fb43b53fa2f28cb6af1eaac1e91e
McChesterson <o.a.onstott@gmail.com>
2023-10-05 15:41:12 -0700

    added particle effects

--- Snow Boarding/Assets/Scripts/LoseDetection.cs
+++ Snow Boarding/Assets/Scripts/LoseDetection.cs
@@ -6,12 +6,18 @@ using UnityEngine.SceneManagement;
 public class LoseDetection : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem deathEffect;
+    bool hasDied = false;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Ground")
+        
+        if (other.tag == "Ground" && hasDied == false)
         {
-            Invoke("ReloadScene", loadDelay); 
+            deathEffect.transform.position = gameObject.transform.position + new Vector3 (0, -1f, 0);
+            deathEffect.Play();
+            Invoke("ReloadScene", loadDelay);
+            hasDied = true;
         }
     }
     void ReloadScene()
